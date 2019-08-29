51342fd8c9739a1825cad28d7442e239c69da739
MR <MR@DESKTOP-SGF5D2F>
2019-08-28 22:35:54 -0500

    retomando los ciclos

---
+++ HolaMundoNombre/src/condicionales/CicloWhile/dowhile.java
@@ -0,0 +1,22 @@
+//ciclo do while
+//do{
+//Instrucciones
+//}while(condiciones);
+package condicionales.CicloWhile;
+
+/**
+ *
+ * @author MR
+ */
+public class dowhile {
+public static void main(String[] args) {
+     int i=1;
+    do{
+            System.out.println(i);
+            i++;
+}while (i<=10);            
+            
+        }
+        
+}
+
---
+++ HolaMundoNombre/src/condicionales/CicloWhile/ej3.java
@@ -0,0 +1,8 @@
+//cada calificación es el equivalente de A, B, C, D o F, y para 
+//incrementar el contador de la calificación apropiada. El programa también imprime en pantalla un
+//resumen del número de estudiantes que recibieron cada calificación. pag 165
+package condicionales.CicloWhile;
+
+public class ej3 {
+    
+}
