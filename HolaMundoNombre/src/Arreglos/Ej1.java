e5a86a4e404ff3889ae46e042f4450ea59bf6123
MR <MR@DESKTOP-SGF5D2F>
2019-09-03 15:57:33 -0500

    ya viene lo mas bueno

---
+++ HolaMundoNombre/src/Arreglos/Ej1.java
@@ -0,0 +1,14 @@
+//leer 5 numeros guardarlos en un arreglo y
+//mostralos en el mismo orden introducido
+package Arreglos;
+
+/**
+ *
+ * @author MR
+ */
+public class Ej1 {
+public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ HolaMundoNombre/src/Arreglos/for2.java
@@ -0,0 +1,11 @@
+package Arreglos;
+public class for2 {
+public static void main(String[] args) {
+String  [] Nombres={"luis","daniel","sergio","jose","leo","meli","ruben","enrique","jose","Hugo"};
+
+    for (String i:Nombres) {
+        System.out.println("Nombres :"+i);    
+    }
+    }
+    
+}
